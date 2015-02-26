ea74836b74c88d7bc6ad242a52dd73a4d18d4642
Juan Chamizo González <juan.chamizo@gmail.com>
2015-02-26 13:47:53 +0100

    a

--- 4-agile-lists/js/al-collection-startup.js
+++ 4-agile-lists/js/al-collection-startup.js
@@ -5,16 +5,16 @@ if (Meteor.isClient) {
 }
 if (Meteor.isServer) {
     //Publicamos autof, de mood que esta disponible para todos
-    Meteor.publish('_al', function() {
+    Meteor.publish('_al', function () {
         return Autol.find()
     })
     sCols = sCols || {}
     var arrToRegister = getAutoListColArray()
         //Instanciamos y publicamos en SERVER cada colección que hemos recuperado (de momento sin restricciones)
-    arrToRegister.forEach(function(colName) {
+    arrToRegister.forEach(function (colName) {
         if (!sCols[colName]) {
             sCols[colName] = new Meteor.Collection(colName) //Instanciamos
-            Meteor.publish(colName, function() { //Publicamos
+            Meteor.publish(colName, function () { //Publicamos
                 console.log('PUBLICANDO ', colName)
                 return sCols[colName].find({})
             })
@@ -29,7 +29,7 @@ function getAutoListColArray() {
         var xcols = Autol.find({
             state: 'active'
         })
-        xcols.forEach(function(key) {
+        xcols.forEach(function (key) {
             try {
                 var nLCol = key.content.list.sources.main.collection
                 var nLJoinCol = _.keys(key.content.list.sources)
@@ -40,7 +40,7 @@ function getAutoListColArray() {
                 arrToRegister.push(_.underscored(nLCol))
             }
             if (nLJoinCol) {
-                nLJoinCol.forEach(function(k) {
+                nLJoinCol.forEach(function (k) {
                     // console.log(k)
                     if (k != 'main') {
                         arrToRegister.push(_.underscored(k))
@@ -54,7 +54,7 @@ function getAutoListColArray() {
     //Nos subscribimos a Autol, para poder usarlo en todas partes
     //FIXME Esto debería ser de solo lectura si el user no es admin @security
 if (Meteor.isClient) {
-    Meteor.subscribe('_al', function() {
+    Meteor.subscribe('_al', function () {
         loadAutoCollection()
         Session.set('AutolLoad', 1)
     })
@@ -62,10 +62,10 @@ if (Meteor.isClient) {
 
 function loadAutoCollection() {
     var arrToRegister = getAutoListColArray()
-    arrToRegister.forEach(function(colName) {
+    arrToRegister.forEach(function (colName) {
         if (!cCols[colName]) {
             cCols[colName] = new Meteor.Collection(colName)
-            Meteor.subscribe(colName, function() {})
+            Meteor.subscribe(colName, function () {})
         }
     })
 }
