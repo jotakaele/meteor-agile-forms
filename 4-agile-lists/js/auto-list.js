314dc2b0d7fa0b3e951c5dd9e5b3140bf5bb9fe1
Juan Chamizo González <juan.chamizo@gmail.com>
2015-03-03 08:38:26 +0100

    joins ok

--- 4-agile-lists/js/auto-list.js
+++ 4-agile-lists/js/auto-list.js
@@ -50,6 +50,12 @@ autol = function autol(options) {
             }
             var bodyF = '' //El cuerpo de la funcion a crear al vuelo
             var originalFields = value.options.fields
+            var keysToKeep = _.keys(originalFields) //Los campos indicados expresamente
+            keysToKeep.push('_id') //el campo _id 
+            if (value.relation) { //Si hay value options
+                keysToKeep.push(value.relation.self)
+                tmpObj.fields[value.relation.self] = 1
+            }
             _.each(originalFields, function (trValue, trKey) {
                     // dbg(trKey, trValue)
                     bodyF += 'doc.' + trKey + '=' + trValue.replace(/@/g, 'doc.') + ';\n'
@@ -59,22 +65,48 @@ autol = function autol(options) {
                     })
                 })
                 // dbg("tmpObj", o2S(tmpObj))
-            _.extend(value.options, tmpObj);
+            _.extend(value.options, tmpObj, {
+                fields: {}
+            });
             // dbg("value.options", value.options)
             ///
             ///
             ///
             ///
             //Ejecutamos la query y la anexamos a value.data
-            value.data = masterConnection[value.collection].find(value.selector || {}, value.options || {}).fetch()
+            value.tempData = masterConnection[value.collection].find(value.selector || {}, value.options || {}).fetch()
                 //dbg('originalFields', originalFields)
-            value.filteredData = value.data.map(function (record) {
-                    return _.pick(record, _.keys(originalFields))
-                })
+            value.data = value.tempData.map(function (record) {
+                return _.pick(record, keysToKeep)
+            })
+            delete value.tempData
                 //Eliminamos los campos que no se han pedido en la query
-                /*value.data.map(function (key) {
-                    value.data[key] = _.pick(value.data[key], _.keys(originalFields))
-                })*/
+        })
+    }
+    this.mergeToMain = function () {
+        dbg("this.list", this.list)
+        var parent = this
+        _.each(this.list.sources, function (value, key) {
+            if (key != 'main') {
+                var sJoin = value.relation
+                var relationSource = sJoin.source.split('@')[0]
+                var relationKey = sJoin.source.split('@')[1]
+                var data = value.data
+                var tmpData = {}
+                _.each(data, function (record) {
+                    if (!tmpData[record[sJoin.self]]) {
+                        tmpData[record[sJoin.self]] = {}
+                    }
+                    //tmpData[record[sJoin.self]][record['_id']] = _.omit(record, sJoin.self, '_id')
+                    tmpData[record[sJoin.self]][record['_id']] = _.omit(record, sJoin.self, '_id')
+                })
+                var mainData = parent.list.sources[relationSource].data
+                mainData.map(function (mainRecord) {
+                    mainRecord[key] = tmpData[mainRecord[relationKey]] || {}
+                })
+                dbg("tmpData", tmpData)
+                dbg("mainData", o2S(mainData))
+            }
         })
     }
 }
